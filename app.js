9a4f2a74e94912fe95c258e3a6c74423cfc515cd
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-08-19 01:15:12 +0100

    added more routes and a general function to the app

--- app.js
+++ app.js
@@ -1,79 +1,24 @@
-const express = require('express');
-const { required } = require('joi');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5500;
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const home = require("./routes/home");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
-app.use(express.json());
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to Vidly database..."))
+  .catch((err) =>{return console.error(err.message)})
 
-const genres = [
-    {id : 1, genretype : 'horror', title : 'Horror'},
-    {id : 2, genretype : 'comedy', title : 'Piral'},
-    {id : 3, genretype : 'action', title : 'Dinosaur'},
-    {id : 4, genretype : 'adventure', title : 'Journal'},
-    {id : 5, genretype : 'romance', title : 'Art'},
-]
+app.use("/api/rentals",rentals);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/genres", genres);
+app.use("/", home);
 
-
-app.get('/', (req, res) => {
-    res.status(200).send("Welcome to Vidly")
-})
-
-app.get('/api/genres', (req, res) => {
-    res.status(200).send(genres);
-})
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find((g) =>  g.id === parseInt(req.params.id))
-    if (!genre) res.status(404).send("Genre with the given id not found")
-    else return res.status(200).send(genre);
-})
-
-app.post('/api/genres', (req, res) => {
-    const genre = {
-        id: genres.length+1,
-        genreType: req.body.genreType,
-        title : req.body.title
-    }
-    const {error, value} = validateGenre(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-    else {
-        genres.push(genre)
-        return res.status(200).send(value);
-    }
-})
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send("The given genre id is not found in record")
-    const {error, value} = validateGenre(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-    else {
-        genre.genretype = req.body.genreType
-        genre.title = req.body.title
-        return res.status(200).send(value);
-    }
-})
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send("The given genre id is not found in record")
-    else {
-        const index = genres.indexOf(genre)
-        genres.splice(index,1)
-        return res.status(200).send(genre);
-    }
-})
-
-function validateGenre(genre) {
-    const schema = Joi.object({
-        genreType: Joi.string().alphanum().min(3).required(),
-        title: Joi.string().min(5).required()        
-    })
-
-    const validResult = schema.validate(genre);
-
-    return validResult
-}
-
-app.listen(port, () =>"Listening on port" + port + "...");  
+app.listen(port, () => "Listening on port" + port + "...");
