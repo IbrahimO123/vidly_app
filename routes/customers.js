a15d897867fc89288f338590039cd0e3c2a71c86
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-09-08 06:02:24 +0100

    routes for accessing the customers for the app

--- routes/customers.js
+++ routes/customers.js
@@ -1,70 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const validateCustomer = require("../functions/validateCustomers");
-const {Customers,} = require("../schemas/customerSchema");
+const authWare = require("../middleware/authWare");
+const { Customers } = require("../schemas/customerSchema");
 
 router.use(express.json());
 
 router.get("/", async (req, res) => {
-  try {
-    const getCustomers = await Customers.find();
-    if (!getCustomers)
-      return res.status(404).send("No Database found for customers");
-    res.status(200).send(getCustomers);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+  const getCustomers = await Customers.find();
+  if (!getCustomers)
+    return res.status(404).send("No Database found for customers");
+  res.status(200).send(getCustomers);
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const getCustomer = await Customers.findById(req.params.id);
-    if (!getCustomer)
-      return res.status(404).send("Customer with the given id not found");
-    else {
-      return res.status(200).send(getCustomer);
-    }
-  } catch (error) {
-    res.status(400).send(error.message);
+  const getCustomer = await Customers.findById(req.params.id);
+  if (!getCustomer)
+    return res.status(404).send("Customer with the given id not found");
+  else {
+    return res.status(200).send(getCustomer);
   }
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const { error, value } = validateCustomer(req.body);
-    if (error) return res.status(404).send(error.message);
-    const insertCustomer = await Customers.insertMany([req.body]);
-    if(!insertCustomer) return res.status(404).send("No database exists for customers");
-    return res.status(200).send(insertCustomer);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+  const { error, value } = validateCustomer(req.body);
+  if (error) return res.status(404).send(error.message);
+  const insertCustomer = await Customers.insertMany([req.body]);
+  if (!insertCustomer)
+    return res.status(404).send("No database exists for customers");
+  return res.status(200).send(insertCustomer);
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const { error, value } = validateCustomer(req.body);
-    if (error) return res.status(404).send(error.message);
-    if (!req.params.id) return res.status(404).send("Insert the required id");
-    const updateCustomer = await Customers.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    if (!updateCustomer) return res.status(404).send("Customer not found");
-    return res.status(200).send(updateCustomer);
-  } catch (error) {
-    return res.status(400).send(error.message);
-  }
+router.put("/:id", authWare, async (req, res) => {
+  const { error, value } = validateCustomer(req.body);
+  if (error) return res.status(404).send(error.message);
+  if (!req.params.id) return res.status(404).send("Insert the required id");
+  const updateCustomer = await Customers.findByIdAndUpdate(
+    req.params.id,
+    req.body
+  );
+  if (!updateCustomer) return res.status(404).send("Customer not found");
+  return res.status(200).send(updateCustomer);
 });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const removeCustomer = await Customers.findByIdAndRemove(req.params.id);
-    if(!removeCustomer) return res.status(404).send("Customer not found")
-    return res.status(200).send(removeCustomer);
-  } catch (err) {
-    return res.status(400).send(err.message);
-  }
+router.delete("/:id", authWare, async (req, res) => {
+  const removeCustomer = await Customers.findByIdAndRemove(req.params.id);
+  if (!removeCustomer) return res.status(404).send("Customer not found");
+  return res.status(200).send(removeCustomer);
 });
 
 module.exports = router;
