0fe132fdb945d7068bac407ede8af341ed146de0
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-09-08 05:54:43 +0100

    refractor the app file and also save the return as a variable

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
+const { urlencoded } = require('express');
 const app = express();
-const mongoose = require("mongoose");
+
+app.use(urlencoded({extended: false}));
+
+require("express-async-errors");
+require("./startup/config")();
+require("./startup/logging")();
+require("./startup/dB")();
+require("./startup/routes")(app);
+require("/startup/prod")(app);
+
 const port = process.env.PORT || 5500;
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const home = require("./routes/home");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
 
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to Vidly database..."))
-  .catch((err) =>{return console.error(err.message)})
+//app listening
+const server = app.listen(port, () => winston.info("Listening on port " + port + "..."));
 
-app.use("/api/rentals",rentals);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/genres", genres);
-app.use("/", home);
-
-app.listen(port, () => "Listening on port" + port + "...");
+module.exports = server;
